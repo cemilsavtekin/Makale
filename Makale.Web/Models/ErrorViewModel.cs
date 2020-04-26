89158e997f69e13638f65a5e62fd20f3108a91fb
Cemil Savtekin <cemilsavtekin@hotmail.com>
2020-04-26 18:58:31 +0300

    First Commit

---
+++ Makale.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Makale.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
